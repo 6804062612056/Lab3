f1dd78be0f2d9ebdb3c86492390821ff636d3477
6804062612056 <s6804062612056@email.kmutnb.ac.th>
2025-12-09 15:57:45 +0700

    แก้โค้ด

--- clock.c
+++ clock.c
@@ -5,22 +5,17 @@ int main() {
 	scanf("%d:%d:%d",&h,&m,&s);
 	scanf("%d",&next);
 	
-	if ((s += next) >= 60) {
-		s -= 60;
-		m += 1;
-		if (s >= 60) {
-			s -= 60;
-			m += 1;
-		}	
+	if ((m += next) >= 60) {
+		m -= 60;
+		h += 1;
 		if (m >= 60) {
 			m -= 60;
 			h += 1;
-			if (h >= 24) {
-				h -= 24;
+		}
+		if (h >= 24) {
+			h -= 24;
 			}
 		}
-	}
-	
 	printf("hour:%d\n",h);
 	printf("minute:%d\n",m);
 	printf("second:%d",s);
